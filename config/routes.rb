445c674782d0255a316c297f7b67bb80b7627b09
Hemal Varambhia <hvarambhia@bookingbug.com>
2019-03-13 11:04:22 +0000

    Added a route to a JSON RESTful endpoint that returns canned 5xx responses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ServerSideErrorApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  post 'notify', to: 'notifications#notify'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -55,4 +56,6 @@ ServerSideErrorApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  
 end
