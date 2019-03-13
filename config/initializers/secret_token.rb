7a08e7a29065344989d5e74d3c1d7abcac83e3bf
Hemal Varambhia <hvarambhia@bookingbug.com>
2019-03-13 10:40:58 +0000

    Initial commit - this is a rails app that we return canned HTTP 5xx server side errors.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerSideErrorApp::Application.config.secret_token = 'edeafa8f6e5608aacbd01dfa4e1a1e22eab368672d2ea616205194bcbed72a77f27a2e9b63b541b8b89a9a3ad4a72da3e8d63c97e653866d5c90653a2431bd82'
