215de0484c047341bd6825722d7bf12caf4dc3ca
Hemal Varambhia <hvarambhia@bookingbug.com>
2019-03-18 17:13:31 +0000

    Added a new patch endpoint for webhook testing.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,4 +2,8 @@ class NotificationsController < ApplicationController
   def notify
     render json: { 'error' => 'Server Side Error' }, status: 500
   end
+
+  def patch_notify
+    render json: { 'message' =>  'PATCHed successfully' }, status: 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ServerSideErrorApp::Application.routes.draw do
   post 'notify', to: 'notifications#notify'
+  patch 'patch_notify', to: 'notifications#patch_notify'
 end
